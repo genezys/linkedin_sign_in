0da6903239f788425a18dd0a8d725cf57b6735e5
Vincent Robert <vincent.robert@genezys.net>
2019-03-20 20:50:06 +0100

    Pass OAuth2 access token request errors through to the app
    Rescue unhandled OAuth2::Error for access token requests and pass to the
    app just like authorization request errors.

--- app/controllers/linkedin_sign_in/callbacks_controller.rb
+++ app/controllers/linkedin_sign_in/callbacks_controller.rb
@@ -2,7 +2,7 @@ require_dependency 'linkedin_sign_in/redirect_protector'
 
 class LinkedinSignIn::CallbacksController < LinkedinSignIn::BaseController
   def show
-    redirect_to proceed_to_url, flash: linkedin_sign_in_response
+    redirect_to proceed_to_url, flash: { linkedin_sign_in: linkedin_sign_in_response }
   rescue LinkedinSignIn::RedirectProtector::Violation => error
     logger.error error.message
     head :bad_request
@@ -15,10 +15,12 @@ class LinkedinSignIn::CallbacksController < LinkedinSignIn::BaseController
 
     def linkedin_sign_in_response
       if valid_request? && params[:code].present?
-        { linkedin_sign_in: { token: token } }
+        { token: token }
       else
-        { linkedin_sign_in: { error: error_message } }
+        { error: error_message_for(params[:error]) }
       end
+    rescue OAuth2::Error => error
+      { error: error_message_for(error.code) }
     end
 
     def valid_request?
@@ -29,7 +31,7 @@ class LinkedinSignIn::CallbacksController < LinkedinSignIn::BaseController
       client.auth_code.get_token(params[:code]).token
     end
 
-    def error_message
-      params[:error].presence_in(LinkedinSignIn::OAUTH2_ERRORS) || "invalid_request"
+    def error_message_for(error_code)
+      error_code.presence_in(LinkedinSignIn::OAUTH2_ERRORS) || "invalid_request"
     end
 end
--- test/controllers/callbacks_controller_test.rb
+++ test/controllers/callbacks_controller_test.rb
@@ -5,7 +5,7 @@ class LinkedinSignIn::CallbacksControllerTest < ActionDispatch::IntegrationTest
     post linkedin_sign_in.authorization_url, params: { proceed_to: 'http://www.example.com/login' }
     assert_response :redirect
 
-    stub_token_request code: '4/SgCpHSVW5-Cy', access_token: 'ya29.GlwIBo', id_token: 'eyJhbGciOiJSUzI'
+    stub_token_for '4/SgCpHSVW5-Cy', access_token: 'ya29.GlwIBo', id_token: 'eyJhbGciOiJSUzI'
 
     get linkedin_sign_in.callback_url(code: '4/SgCpHSVW5-Cy', state: flash[:state])
     assert_redirected_to 'http://www.example.com/login'
@@ -13,8 +13,9 @@ class LinkedinSignIn::CallbacksControllerTest < ActionDispatch::IntegrationTest
     assert_nil flash[:linkedin_sign_in][:error]
   end
 
-  LinkedinSignIn::OAUTH2_ERRORS.each do |error|
-    test "receiving an authorization error: #{error}" do
+  # Authorization request errors: https://tools.ietf.org/html/rfc6749#section-4.1.2.1
+  %w[ invalid_request unauthorized_client access_denied unsupported_response_type invalid_scope server_error temporarily_unavailable ].each do |error|
+    test "receiving an authorization code grant error: #{error}" do
       post linkedin_sign_in.authorization_url, params: { proceed_to: 'http://www.example.com/login' }
       assert_response :redirect
 
@@ -43,6 +44,21 @@ class LinkedinSignIn::CallbacksControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to 'http://www.example.com/login'
     assert_nil flash[:linkedin_sign_in][:token]
     assert_equal 'invalid_request', flash[:linkedin_sign_in][:error]
+  end
+
+  # Access token request errors: https://tools.ietf.org/html/rfc6749#section-5.2
+  %w[ invalid_request invalid_client invalid_grant unauthorized_client unsupported_grant_type invalid_scope ].each do |error|
+    test "receiving an access token request error: #{error}" do
+      post linkedin_sign_in.authorization_url, params: { proceed_to: 'http://www.example.com/login' }
+      assert_response :redirect
+
+      stub_token_error_for '4/SgCpHSVW5-Cy', error: error
+
+      get linkedin_sign_in.callback_url(code: '4/SgCpHSVW5-Cy', state: flash[:state])
+      assert_redirected_to 'http://www.example.com/login'
+      assert_nil flash[:linkedin_sign_in][:id_token]
+      assert_equal error, flash[:linkedin_sign_in][:error]
+    end
   end
 
   test "protecting against CSRF without flash state" do
@@ -86,11 +102,27 @@ class LinkedinSignIn::CallbacksControllerTest < ActionDispatch::IntegrationTest
   end
 
   private
-    def stub_token_request(code:, **params)
-      stub_request(:post, 'https://www.linkedin.com/oauth/v2/accessToken').
-        with(body: { grant_type: 'authorization_code', code: code,
-          client_id: FAKE_LINKEDIN_CLIENT_ID, client_secret: FAKE_LINKEDIN_CLIENT_SECRET,
-          redirect_uri: 'http://www.example.com/linkedin_sign_in/callback' }).
-        to_return(status: 200, headers: { 'Content-Type' => 'application/json' }, body: JSON.generate(params))
+    def stub_token_for(code, **response_body)
+      stub_token_request(code, status: 200, response: response_body)
+    end
+
+    def stub_token_error_for(code, error:)
+      stub_token_request(code, status: 418, response: { error: error })
+    end
+
+    def stub_token_request(code, status:, response:)
+      stub_request(:post, 'https://www.linkedin.com/oauth/v2/accessToken').with(
+        body: {
+          grant_type: 'authorization_code',
+          code: code,
+          client_id: FAKE_LINKEDIN_CLIENT_ID,
+          client_secret: FAKE_LINKEDIN_CLIENT_SECRET,
+          redirect_uri: 'http://www.example.com/linkedin_sign_in/callback'
+        }
+      ).to_return(
+        status: status,
+        headers: { 'Content-Type' => 'application/json' },
+        body: JSON.generate(response)
+      )
     end
 end
